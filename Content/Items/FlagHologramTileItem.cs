a5f09fb7d64350547dd80f67b08d50fe5eddafa7
Pixelnando <darkmantis3@gmail.com>
2025-06-01 18:31:01 -0300

    Atualização What a Calamity!

---
+++ Content/Items/FlagHologramTileItem.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using CalamityModPTBR.Content.Tiles;
+
+namespace CalamityModPTBR.Content.Items
+{
+    public class FlagHologramTileItem : ModItem
+    {
+        public override void SetDefaults()
+        {
+            if (ModLoader.TryGetMod("CalamityMod", out Mod calamityMod) && calamityMod.TryFind("LabHologramProjectorItem", out ModItem LabHologramProjector))
+            {
+                Item.CloneDefaults(LabHologramProjector.Type);
+                Item.createTile = ModContent.TileType<FlagHologramTile>();
+            }
+        }
+        public override void AddRecipes()
+        {
+            if (ModLoader.TryGetMod("CalamityMod", out Mod calamityMod) && calamityMod.TryFind("LaboratoryPlating", out ModItem LaboratoryPlating) && calamityMod.TryFind("DubiousPlating", out ModItem DubiousPlating))
+            {
+                CreateRecipe().
+                    AddIngredient(LaboratoryPlating.Type, 10).
+                    AddIngredient(DubiousPlating.Type, 5).
+                    AddTile(TileID.Anvils).
+                Register();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+![Logo do projeto](WorkshopImages/readmeImage.png)
+
+Este mod fornece uma tradução não-oficial do Calamity Mod para o Português Brasileiro.
+
+# Créditos
+
+- Lore e entradas no bestiário traduzidas por StipulateVenus.
+- Tradução feita por Pixelnando.
