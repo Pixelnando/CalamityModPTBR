3bc7a3771eeabce419928e2385d7f9f40a40db52
Pixelnando <darkmantis3@gmail.com>
2025-09-01 17:56:03 -0300

    Nova config

---
+++ TranslationConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader.Config;
+
+namespace CalamityModPTBR
+{
+    public class TranslationConfig : ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ClientSide;
+
+        [Header("$Mods.CalamityModPTBR.Configs.TranslationConfig.Header")]
+        [LabelKey("$Mods.CalamityModPTBR.Configs.TranslationConfig.MostrarNomeOriginal.Label")]
+        [TooltipKey("$Mods.CalamityModPTBR.Configs.TranslationConfig.MostrarNomeOriginal.Tooltip")]
+        [DefaultValue(false)]
+        public bool mostrarNomeOriginal;
+    }
+}
---
+++ TranslationGlobalItem.cs
@@ -0,0 +1,35 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using System.Collections.Generic;
+
+namespace CalamityModPTBR
+{
+    public class TranslationGlobalItem : GlobalItem
+    {
+        public override bool InstancePerEntity => true;
+
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+        {
+            var config = ModContent.GetInstance<TranslationConfig>();
+
+            if (!config.mostrarNomeOriginal)
+                return;
+
+            foreach (var line in tooltips)
+            {
+                if (line.Mod == "Terraria" && line.Name == "ItemName")
+                {
+                    if (item.ModItem != null && item.ModItem.Mod.Name == "CalamityMod")
+                    {
+                        string internalName = ItemLoader.GetItem(item.type).Name;
+                        line.Text = $"{line.Text} ({internalName})";
+                    }
+                    else
+                    {
+                        string internalName = item.Name;
+                    }
+                }
+            }
+        }
+    }
+}
